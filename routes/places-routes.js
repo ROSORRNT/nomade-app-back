a1c8b7b9aa16b59194ce514d5c658a93081a4191
Romain <romainsorrnti@gmail.com>
2020-02-17 14:47:16 +0100

    Implementing Basic Routing

---
+++ routes/places-routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  console.log('GET request in Places')
+  res.json({ message: "it's works" })
+})
+
+module.exports = router
