d37d3f672a5acda2bc144967dfd3a06f69b87d6a
Romain <romainsorrnti@gmail.com>
2020-02-17 15:52:03 +0100

    Adding Place specific routes + Getting a place by user ID

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,34 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-  console.log('GET request in Places')
-  res.json({ message: "it's works" })
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Musée du Louvre',
+    description:
+      "Le Louvre possède une longue histoire de conservation artistique et historique, depuis l'Ancien Régime jusqu'à nos jours.",
+    location: {
+      lat: 48.860608,
+      lng: 2.337642,
+    },
+    address: 'Rue de Rivoli, 75001 Paris',
+    creator: 'u1',
+  },
+]
+// ex: => api/places/p1
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid // { pid: 'p1' }
+  const place = DUMMY_PLACES.find(p => p.id === placeId)
+
+  res.json({ place })
+})
+
+router.get('/user/:uid', (req, res, next) => {
+  const userId = req.params.uid
+
+  const place = DUMMY_PLACES.find(p => p.creator === userId)
+
+  res.json({ place })
 })
 
 module.exports = router
