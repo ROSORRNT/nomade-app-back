3e9891753a265dd24fa6367c1784e54228a08b1e
Romain <romainsorrnti@gmail.com>
2020-02-18 16:37:15 +0100

    Handling Errors TODO // do it with no replicated code

--- app.js
+++ app.js
@@ -8,4 +8,12 @@ const app = express()
 
 app.use('/api/places', placesRoutes) // => app/places/...
 
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error)
+  }
+  res.status(error.code || 500)
+  res.json({ message: error.message || "Une erreur inconnue s'est produite." })
+})
+
 app.listen(5000)
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -7,7 +7,7 @@ const DUMMY_PLACES = [
     id: 'p1',
     title: 'Musée du Louvre',
     description:
-      "Le Louvre possède une longue histoire de conservation artistique et historique, depuis l'Ancien Régime jusqu'à nos jours.",
+      "Le Louvre possède une longue histoire de conser vation artistique et historique, depuis l'Ancien Régime jusqu'à nos jours.",
     location: {
       lat: 48.860608,
       lng: 2.337642,
@@ -21,14 +21,27 @@ router.get('/:pid', (req, res, next) => {
   const placeId = req.params.pid // { pid: 'p1' }
   const place = DUMMY_PLACES.find(p => p.id === placeId)
 
+  if (!place) {
+    const error = new Error("Aucun lieu trouvé pour l'identifiant fourni.")
+    error.code = 404
+    // throw cancel the function execution, next() does not (so, return)
+    return next(error)
+  }
+
   res.json({ place })
 })
 
 router.get('/user/:uid', (req, res, next) => {
   const userId = req.params.uid
-
   const place = DUMMY_PLACES.find(p => p.creator === userId)
 
+  if (!place) {
+    const error = new Error(
+      "Aucun lieu trouvé pour l'identifiant utilisateur fourni."
+    )
+    error.code = 404
+    return next(error)
+  }
   res.json({ place })
 })
 
