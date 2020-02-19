30c931bc2b28b57eb443b2534a83018190eb10f2
Romain <romainsorrnti@gmail.com>
2020-02-19 13:58:21 +0100

    Handling Errors for unsupported routes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const placesRoutes = require('./routes/places-routes')
-const usersRoutes = require('./routes/users-routes')
+const HttpError = require('./models/http-error')
 
 const app = express()
 
@@ -12,6 +12,11 @@ so, in createPlace (places-controllers) we are now able to get the parse body
 app.use(bodyParser.json())
 
 app.use('/api/places', placesRoutes) // => app/places/...
+
+app.use((req, res, next) => {
+  const error = new HttpError("Cette route n'a pu être trouvée.", 404)
+  return next(error)
+})
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
