ba7a693183416280b5c1a673d35116fdef83509a
Romain <romainsorrnti@gmail.com>
2020-02-19 17:45:50 +0100

    Validating api inputs (requests bodies)

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,5 @@
 const uuid = require('uuid/v4')
+const { validationResult } = require('express-validator')
 
 const HttpError = require('../models/http-error')
 
@@ -48,7 +49,17 @@ const getPlacesByUserId = (req, res, next) => {
 }
 
 const createPlace = (req, res, next) => {
+  const error = validationResult(req)
+  if (!error.isEmpty()) {
+    console.log(error)
+    // res.status(422)
+    throw new HttpError(
+      'Entrées de formulaire invalides, veuillez vérifiez vos données.',
+      422
+    )
+  }
   const { title, description, coordinates, address, creator } = req.body
+
   const createdPlace = {
     id: uuid(),
     title,
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,15 +1,28 @@
-const express = require('express')
+const { Router } = require('express')
+const { check } = require('express-validator')
 
 const placesControllers = require('../controllers/places-controllers')
 
-const router = express.Router()
+const router = Router()
 
 // ex: => api/places/p1
 router.get('/:pid', placesControllers.getPlaceById)
 
 router.get('/user/:uid', placesControllers.getPlacesByUserId)
 
-router.post('/', placesControllers.createPlace)
+router.post(
+  '/',
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 }),
+    check('address')
+      .not()
+      .isEmpty(),
+  ],
+  placesControllers.createPlace
+)
 
 router.patch('/:pid', placesControllers.updatePlaceById)
 
