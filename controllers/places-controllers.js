2ab2dc6129e3b356ea69495cb0ce25daafe21c51
Romain <romainsorrnti@gmail.com>
2020-02-20 15:35:23 +0100

    Updating Places

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -106,7 +106,7 @@ const createPlace = async (req, res, next) => {
   res.status(201).json({ place: createdPlace })
 }
 
-const updatePlaceById = (req, res, next) => {
+const updatePlaceById = async (req, res, next) => {
   const error = validationResult(req)
   if (!error.isEmpty()) {
     throw new HttpError(
@@ -117,14 +117,31 @@ const updatePlaceById = (req, res, next) => {
   const { title, description } = req.body
   const placeId = req.params.pid
 
-  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) }
-  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId)
-  updatedPlace.title = title
-  updatedPlace.description = description
+  let place
+  try {
+    place = await Place.findById(placeId)
+  } catch (err) {
+    const error = new HttpError(
+      "Quelque chose s'est mal passé, le lieu n'a pu être édité.",
+      500
+    )
+    return next(error)
+  }
 
-  DUMMY_PLACES[placeIndex] = updatedPlace
+  place.title = title
+  place.description = description
 
-  res.status(200).json({ place: updatedPlace })
+  try {
+    await place.save()
+  } catch (err) {
+    const error = new HttpError(
+      "Quelque chose s'est mal passé, le lieu n'a pu être édité.",
+      500
+    )
+    return next(error)
+  }
+
+  res.status(200).json({ place: place.toObject({ getters: true }) })
 }
 
 const deletePlaceById = (req, res, next) => {
