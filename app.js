9778075e4afb35653f3a560f5baf9894988b4e64
Romain <romainsorrnti@gmail.com>
2020-02-20 14:12:27 +0100

    Connecting backend to MongoDB with Mongoose + Creating the Place Schema model

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const placesRoutes = require('./routes/places-routes')
 const usersRoutes = require('./routes/users-routes')
@@ -7,9 +8,6 @@ const HttpError = require('./models/http-error')
 
 const app = express()
 
-/* this will parse any incomming requests body, extract json data converted to regular JS, and then call next() automatically, so be reached the next middleware and then also add the json() data :
-so, in createPlace (places-controllers) we are now able to get the parse body
- */
 app.use(bodyParser.json())
 
 app.use('/api/places', placesRoutes) // => app/places/...
@@ -28,4 +26,11 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "Une erreur inconnue s'est produite." })
 })
 
-app.listen(5000)
+mongoose
+  .connect(
+    'mongodb+srv://rosorrnti:Romainsorrenti06@cluster0-powk1.mongodb.net/places?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    app.listen(5000)
+  })
+  .catch(err => console.log(err))
